21fa97c0442d20b2f1645e3a647a85b6a6e673ae
Deny Febriyanto <denyfebri@gmail.com>
2025-06-03 22:47:32 +0700

    first init

---
+++ Variable/src/com/tutorial/Main.java
@@ -0,0 +1,17 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        //Tipe data & variable
+        int a=10;
+        System.out.printf("Nilai a = %d \n",a);
+        
+        a=20;
+        System.out.printf("Nilai a baru = %d \n",a);
+        
+        //Deklarasi
+        int b;
+        b=23;
+        System.out.println("Nilai b = "+b);
+    }
+}
